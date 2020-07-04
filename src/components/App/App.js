b69196b51ef03da4793a5d502f4ca09c8f17facb
melaniesarah <melaniesarah@gmail.com>
2020-07-03 21:13:30 -0500

    Integrate other components into App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
 }
 
 export default App;
