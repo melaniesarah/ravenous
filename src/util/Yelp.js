c65555675ca0434c14d73fafa420c4c619b9e69c
melaniesarah <melaniesarah@gmail.com>
2020-07-07 01:05:05 -0500

    Add query to yelp api functionality

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,5 +1,41 @@
 import {API_KEY as apiKey} from './config';
 
 const Yelp = {
+  search: async (term, location, sortBy) => {
+    const url = 'https://api.yelp.com/v3/businesses/search?';
+    const termQuery = 'term=' + term;
+    const locationQuery = '&location=' + location;
+    const sortByQuery = '&sort_by=' + sortBy;
+    const corsUrl = 'https://cors-anywhere.herokuapp.com/';
+    const endpoint = corsUrl + url + termQuery + locationQuery + sortByQuery;
+
+    try {
+        const response = await fetch(endpoint, { headers: { Authorization: `Bearer ${apiKey}` }});
     
+        if (response.ok) {
+            const jsonResponse = await response.json();
+
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating, 
+                        reviewCount: business.review_count
+                    }
+                })
+            }  
+        } 
+    } catch (error) {
+        console.log(error);
+    }  
+  }
 };
+
+export default Yelp;
