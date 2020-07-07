fe83cc48ae52f3696ef2ce8427f17ae69111e9d5
melaniesarah <melaniesarah@gmail.com>
2020-07-07 01:37:29 -0500

    Complete integration of yelp api search

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,23 +4,21 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
+import Yelp from '../../util/Yelp';
 
-const businesses = [business, business, business, business, business, business];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: []
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
   searchYelp(term, location, sortBy) {
-    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+    Yelp.search(term, location, sortBy)
+    .then(businesses => this.setState({ businesses: businesses }));
   }
 
   render() {
@@ -28,7 +26,7 @@ class App extends Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,7 +1,7 @@
-import {API_KEY as apiKey} from './config';
+import { config } from './config.js';
 
 const Yelp = {
-  search: async (term, location, sortBy) => {
+  search: async function(term, location, sortBy) {
     const url = 'https://api.yelp.com/v3/businesses/search?';
     const termQuery = 'term=' + term;
     const locationQuery = '&location=' + location;
@@ -9,33 +9,32 @@ const Yelp = {
     const corsUrl = 'https://cors-anywhere.herokuapp.com/';
     const endpoint = corsUrl + url + termQuery + locationQuery + sortByQuery;
 
-    try {
-        const response = await fetch(endpoint, { headers: { Authorization: `Bearer ${apiKey}` }});
-    
-        if (response.ok) {
-            const jsonResponse = await response.json();
-
-            if (jsonResponse.businesses) {
-                return jsonResponse.businesses.map(business => {
-                    return {
-                        id: business.id,
-                        imageSrc: business.image_url,
-                        name: business.name,
-                        address: business.location.address1,
-                        city: business.location.city,
-                        state: business.location.state,
-                        zipCode: business.location.zip_code,
-                        category: business.categories[0].title,
-                        rating: business.rating, 
-                        reviewCount: business.review_count
-                    }
-                })
-            }  
-        } 
-    } catch (error) {
-        console.log(error);
-    }  
-  }
+    return fetch(endpoint, {
+      headers: { Authorization: `Bearer ${config.API_KEY}` },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (jsonResponse.businesses) {
+          console.log(jsonResponse);
+          return jsonResponse.businesses.map((business) => {
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.address1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count,
+            };
+          });
+        }
+      });
+    }
 };
 
 export default Yelp;
