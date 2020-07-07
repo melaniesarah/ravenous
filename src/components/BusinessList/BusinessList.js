32c0fb54612a0592ea0b75226fa1382ef55a0bc4
melaniesarah <melaniesarah@gmail.com>
2020-07-06 22:44:28 -0500

    Set up logic to wire in Yelp API to App.js file

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -8,12 +8,12 @@ class BusinessList extends Component {
     render() {
         return (
             <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
+                { 
+                    this.props.businesses.map(business => {
+                        return <Business business={business} />;
+                    })
+                }
+               
             </div>
         );
     }
